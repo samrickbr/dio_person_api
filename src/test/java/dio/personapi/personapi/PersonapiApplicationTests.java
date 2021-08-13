04ada59171395963122effa1bba7d165c430f856
samrick <ricrdocardoso@gmail.com>
2021-08-13 16:16:10 -0300

    Inicio do projeto Person API
    Configuração inicial para fazer deploy no Heroku.

---
+++ src/test/java/dio/personapi/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.personapi.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
